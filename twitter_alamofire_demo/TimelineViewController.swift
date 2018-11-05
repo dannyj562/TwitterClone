04da979f6d8b6b39d3919bd6fefa52af8da2516f
dannyj562 <riveradanny51@gmail.com>
2018-11-05 01:00:31 -0800

    Add pull to refresh control

--- twitter_alamofire_demo/TimelineViewController.swift
+++ twitter_alamofire_demo/TimelineViewController.swift
@@ -20,6 +20,21 @@ class TimelineViewController: UIViewController, UITableViewDelegate, UITableView
         self.tableView.dataSource = self
         self.tableView.rowHeight = UITableViewAutomaticDimension
         self.tableView.estimatedRowHeight = 100
+        
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(refreshControlAction(_:)), for: UIControlEvents.valueChanged)
+        refreshControl.tintColor = UIColor.green
+        tableView.insertSubview(refreshControl, at: 0)
+        
+        fetchTimeLine()
+    }
+    
+    @objc func refreshControlAction(_ refreshControl: UIRefreshControl) {
+        fetchTimeLine()
+        refreshControl.endRefreshing()
+    }
+    
+    func fetchTimeLine() {
         APIManager.shared.getHomeTimeLine { (tweets, error) in
             if let tweets = tweets {
                 self.tweets = tweets
@@ -29,6 +44,7 @@ class TimelineViewController: UIViewController, UITableViewDelegate, UITableView
             }
         }
     }
+    
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return tweets.count
